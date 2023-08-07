bed4179b833c4b3478750c506410813df0307e8a
DekelA <dekel16791@gmail.com>
2023-08-07 11:08:19 +0300

    Update

---
+++ OrdersAppSolution/OrdersApp.Core/Entities/OrderItem.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrdersApp.Core.Entities
+{
+    public class OrderItem
+    {
+        #region Properties
+
+        [Key]
+        public Guid OrderItemID { get; set; }
+
+        [Required(ErrorMessage = "OrderID can't be blank")]
+        public Guid OrderID { get; set; }
+
+        [Required(ErrorMessage = "Product Name can't be blank")]
+        [StringLength(50, ErrorMessage = "The Product Name field must not exceed 50 characters.")]
+        public string? ProductName { get; set; }
+
+        [Required(ErrorMessage = "Product Name can't be blank")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity has to have a positive value")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Unit Price can't be blank")]
+        [Range(0, double.MaxValue, ErrorMessage = "Unit Price has to have a positive value")]
+        public decimal? UnitPrice { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Total Price has to have a positive value")]
+        public decimal? TotalPrice { get; set; }
+
+        #endregion
+    }
+}
